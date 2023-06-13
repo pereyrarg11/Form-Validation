855a12164defc9aea8a2ef26d50c6338d1ef05df
Gabriel Pereyra R <pereyrarg11@gmail.com>
2023-06-12 21:07:02 -0600

    chore: validate on submit

--- app/src/main/java/com/pereyrarg11/formvalidation/presentation/MainViewModel.kt
+++ app/src/main/java/com/pereyrarg11/formvalidation/presentation/MainViewModel.kt
@@ -4,10 +4,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.pereyrarg11.formvalidation.domain.use_case.ValidateEmailUseCase
 import com.pereyrarg11.formvalidation.domain.use_case.ValidatePasswordUseCase
 import com.pereyrarg11.formvalidation.domain.use_case.ValidateRepeatedPasswordUseCase
 import com.pereyrarg11.formvalidation.domain.use_case.ValidateTermsUseCase
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val validateEmailUseCase: ValidateEmailUseCase = ValidateEmailUseCase(),
@@ -16,7 +20,65 @@ class MainViewModel(
     private val validateTermsUseCase: ValidateTermsUseCase = ValidateTermsUseCase(),
 ) : ViewModel() {
 
+    private val validationEventChannel = Channel<ValidationEvent>()
+    val validationEvents = validationEventChannel.receiveAsFlow()
+
     var state by mutableStateOf(RegistrationFormState())
 
+    fun onEvent(event: RegistrationFormEvent) {
+        when (event) {
+            is RegistrationFormEvent.AcceptTerms -> {
+                state = state.copy(acceptedTerms = event.isAccepted)
+            }
 
+            is RegistrationFormEvent.EmailChanged -> {
+                state = state.copy(email = event.email)
+            }
+
+            is RegistrationFormEvent.PasswordChanged -> {
+                state = state.copy(password = event.password)
+            }
+
+            is RegistrationFormEvent.RepeatedPasswordChanged -> {
+                state = state.copy(repeatedPassword = event.repeatedPassword)
+            }
+
+            RegistrationFormEvent.Submit -> {
+                submitData()
+            }
+        }
+    }
+
+    private fun submitData() {
+        val emailResult = validateEmailUseCase(state.email)
+        val passwordResult = validatePasswordUseCase(state.password)
+        val repeatedPasswordResult =
+            validateRepeatedPasswordUseCase(state.password, state.repeatedPassword)
+        val termsResult = validateTermsUseCase(state.acceptedTerms)
+
+        val hasError = listOf(
+            emailResult,
+            passwordResult,
+            repeatedPasswordResult,
+            termsResult,
+        ).any { !it.successful }
+
+        if (hasError) {
+            state = state.copy(
+                emailError = emailResult.errorMessage,
+                passwordError = passwordResult.errorMessage,
+                repeatedPasswordError = repeatedPasswordResult.errorMessage,
+                termsError = termsResult.errorMessage,
+            )
+            return
+        }
+
+        viewModelScope.launch {
+            validationEventChannel.send(ValidationEvent.Success)
+        }
+    }
+
+    sealed class ValidationEvent {
+        object Success : ValidationEvent()
+    }
 }
---
+++ app/src/main/java/com/pereyrarg11/formvalidation/presentation/RegistrationFormEvent.kt
@@ -0,0 +1,9 @@
+package com.pereyrarg11.formvalidation.presentation
+
+sealed class RegistrationFormEvent {
+    data class EmailChanged(val email: String) : RegistrationFormEvent()
+    data class PasswordChanged(val password: String) : RegistrationFormEvent()
+    data class RepeatedPasswordChanged(val repeatedPassword: String) : RegistrationFormEvent()
+    data class AcceptTerms(val isAccepted: Boolean) : RegistrationFormEvent()
+    object Submit : RegistrationFormEvent()
+}
