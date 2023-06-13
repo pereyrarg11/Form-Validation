750714d9607017b9e348dec2a339d9bc2f9c4197
Gabriel Pereyra R <pereyrarg11@gmail.com>
2023-06-12 21:45:33 -0600

    chore: create the registration form

--- app/src/main/java/com/pereyrarg11/formvalidation/presentation/MainActivity.kt
+++ app/src/main/java/com/pereyrarg11/formvalidation/presentation/MainActivity.kt
@@ -6,18 +6,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pereyrarg11.formvalidation.ui.theme.FormValidationTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -50,7 +64,102 @@ class MainActivity : ComponentActivity() {
                             .padding(32.dp),
                         verticalArrangement = Arrangement.Center,
                     ) {
+                        // Email
+                        TextField(
+                            value = state.email,
+                            onValueChange = {
+                                viewModel.onEvent(RegistrationFormEvent.EmailChanged(it))
+                            },
+                            isError = state.emailError != null,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Email")
+                            },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                        )
+                        if (state.emailError != null) {
+                            Text(
+                                text = state.emailError,
+                                color = MaterialTheme.colorScheme.error,
+                                modifier = Modifier.align(Alignment.End),
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
 
+                        // Password
+                        TextField(
+                            value = state.password,
+                            onValueChange = {
+                                viewModel.onEvent(RegistrationFormEvent.PasswordChanged(it))
+                            },
+                            isError = state.passwordError != null,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Password")
+                            },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        )
+                        if (state.passwordError != null) {
+                            Text(
+                                text = state.passwordError,
+                                color = MaterialTheme.colorScheme.error,
+                                modifier = Modifier.align(Alignment.End),
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Repeated password
+                        TextField(
+                            value = state.repeatedPassword,
+                            onValueChange = {
+                                viewModel.onEvent(RegistrationFormEvent.RepeatedPasswordChanged(it))
+                            },
+                            isError = state.repeatedPasswordError != null,
+                            modifier = Modifier.fillMaxWidth(),
+                            placeholder = {
+                                Text(text = "Repeat password")
+                            },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                        )
+                        if (state.repeatedPasswordError != null) {
+                            Text(
+                                text = state.repeatedPasswordError,
+                                color = MaterialTheme.colorScheme.error,
+                                modifier = Modifier.align(Alignment.End),
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Terms
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                        ) {
+                            Checkbox(
+                                checked = state.acceptedTerms,
+                                onCheckedChange = {
+                                    viewModel.onEvent(
+                                        RegistrationFormEvent.AcceptTerms(it)
+                                    )
+                                },
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = "Accept terms")
+                        }
+                        if (state.termsError != null) {
+                            Text(
+                                text = state.termsError,
+                                color = MaterialTheme.colorScheme.error,
+                                modifier = Modifier.align(Alignment.End),
+                            )
+                        }
+
+                        // Submit
+                        Button(
+                            onClick = { viewModel.onEvent(RegistrationFormEvent.Submit) },
+                            modifier = Modifier.align(Alignment.End),
+                        ) {
+                            Text(text = "Submit")
+                        }
                     }
                 }
             }
